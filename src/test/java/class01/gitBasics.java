ee819ba06336382990798ac09664ae54bcb10c15
melos7n <ernest.melo88@gmail.com>
2022-08-24 21:26:00 -0400

    added git ignore file

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,14 @@
+package class01;
+
+public class gitBasics {
+
+    public static void main(String [] arg ){
+        // lets write a code that checks the rempature from a website and then prints
+        // out that it is a beautiful day or not
+
+        System.out.println("today is not a beautiful day");
+
+
+
+    }
+}
